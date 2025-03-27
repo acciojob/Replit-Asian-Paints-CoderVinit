2c46a3d1cfcf8af782096e282217dcd01ec4611c
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-03-27 09:21:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+
+document.getElementById("change_button").addEventListener("click",()=>{
+	let blockId = document.getElementById("block_id").value;
+	let color = document.getElementById("color_id").value;
+
+	document.querySelectorAll(".grid-item").forEach((item)=>{
+		item.style.backgroundColor = "transparent";
+	})
+
+	let selectedBlock = document.getElementById(blockId);
+	if(selectedBlock){
+		selectedBlock.style.backgroundColor=color;
+	}
+	else{
+		alert("Invalid Block ID!")
+	}
+})
+
+document.getElementById('Reset').addEventListener('click', function() {
+    document.querySelectorAll('.grid-item').forEach(item => {
+        item.style.backgroundColor = 'transparent';
+	});
+});
